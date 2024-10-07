41a08503ede75a04cb60490f62202a8bb7c3c336
Duartini <Emanuel-d@hotmail.com>
2024-10-07 15:17:27 -0600

    sudo as user

--- obsidian.sh
+++ obsidian.sh
@@ -5,6 +5,6 @@ echo "Creating forder to installation of Obsidian"
 mkdir -p ~/configuration/obsidian/
 wget -P ~/configuration/obsidian/ https://github.com/obsidianmd/obsidian-releases/releases/download/v1.6.7/Obsidian-1.6.7.AppImage
 chmdo +x ~/configuration/obsidian/Obsidian-1.6.7.AppImage
-sudo ~/configuration/obsidian/Obsidian-1.6.7.AppImage
+sudo -u $SUDO_USER ~/configuration/obsidian/Obsidian-1.6.7.AppImage
 
 echo "Obsidian installed"
