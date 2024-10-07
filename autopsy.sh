ab57ddaf2c7a5d40576a72db60ec6a88288fcdb8
Duartini <Emanuel-d@hotmail.com>
2024-10-07 00:32:14 -0600

    link-raw

--- autopsy.sh
+++ autopsy.sh
@@ -11,13 +11,13 @@ sudo apt remove -y libtsk19
 wget -P ~/configuration/autopsy/ https://github.com/sleuthkit/sleuthkit/releases/download/sleuthkit-4.12.1/sleuthkit-java_4.12.1-1_amd64.deb
 sudo dpkg -i ~/configuration/autopsy/sleuthkit-java_4.12.1-1_amd64.deb
 
-wget -P ~/configuration/autopsy/ https://github.com/sleuthkit/autopsy/blob/develop/linux_macos_install_scripts/install_prereqs_ubuntu.sh
+wget -P ~/configuration/autopsy/ https://raw.githubusercontent.com/sleuthkit/autopsy/refs/heads/develop/linux_macos_install_scripts/install_prereqs_ubuntu.sh
 
 sudo chmod +x ~/configuration/autopsy/install_prereqs_ubuntu.sh
 
 sudo ~/configuration/autopsy/install_prereqs_ubuntu.sh
 
-wget -P ~/configuration/autopsy/ https://github.com/sleuthkit/autopsy/blob/develop/linux_macos_install_scripts/install_application.sh
+wget -P ~/configuration/autopsy/ https://raw.githubusercontent.com/sleuthkit/autopsy/refs/heads/develop/linux_macos_install_scripts/install_application.sh
 
 sudo chmod +x ~/configuration/autopsy/install_application.sh
 
