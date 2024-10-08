4f0121aa9059bf7956c9eb9447daf22cc30ee502
Duartini <Emanuel-d@hotmail.com>
2024-10-08 16:21:34 -0600

    add opt

--- autopsy.sh
+++ autopsy.sh
@@ -7,29 +7,27 @@ wget -P ~/configuration/autopsy/ https://github.com/sleuthkit/autopsy/releases/d
 
 sudo apt remove -y sleuthkit
 sudo apt remove -y libtsk19
-
+sudo apt --fix-broken install -y
 wget -P ~/configuration/autopsy/ https://github.com/sleuthkit/sleuthkit/releases/download/sleuthkit-4.12.1/sleuthkit-java_4.12.1-1_amd64.deb
 sudo dpkg -i ~/configuration/autopsy/sleuthkit-java_4.12.1-1_amd64.deb
-
+sudo apt --fix-broken install -y 
 wget -P ~/configuration/autopsy/ https://raw.githubusercontent.com/sleuthkit/autopsy/refs/heads/develop/linux_macos_install_scripts/install_prereqs_ubuntu.sh
 
 sudo chmod +x ~/configuration/autopsy/install_prereqs_ubuntu.sh
 
 sudo ~/configuration/autopsy/install_prereqs_ubuntu.sh
-
+sudo apt --fix-broken install -y 
+sudo apt install libcanberra-gtk-module libcanberra-gtk3-module -y 
 wget -P ~/configuration/autopsy/ https://raw.githubusercontent.com/sleuthkit/autopsy/refs/heads/develop/linux_macos_install_scripts/install_application.sh
 
 sudo chmod +x ~/configuration/autopsy/install_application.sh
 
 sudo ~/configuration/autopsy/install_application.sh -z ~/configuration/autopsy/autopsy-4.21.0.zip -i ~/autopsy -j /usr/lib/jvm/java-1.17.0-openjdk-amd64
-sudo apt install libcanberra-gtk-module libcanberra-gtk3-module -y 
+
 
 sudo mkdir -p /opt/autopsy
 sudo cp -r ~/autopsy/autopsy-4.21.0 /opt/autopsy/
-echo 'symbolic code of autopsy' >> ~/.bashrc
-echo 'export PATH=$PATH:/opt/autopsy/autopsy-4.21.0/bin/' >> ~/.bashrc
-echo 'symbolic code of autopsy' >> $HOME/.bashrc
-echo 'export PATH=$PATH:/opt/autopsy/autopsy-4.21.0/bin/' >> $HOME/.bashrc
+sudo ln -s /opt/autopsy/bin/autopsy /usr/local/bin/autopsy
 
 #Autopsy installed
 echo "Autopsy installed"
