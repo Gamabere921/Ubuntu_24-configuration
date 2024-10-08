6ebcbcbf37746db0057ce1928b28d92281a406cd
Duartini <Emanuel-d@hotmail.com>
2024-10-08 17:10:49 -0600

    balena2

--- todos.sh
+++ todos.sh
@@ -58,6 +58,30 @@ sudo dpkg -i ~/configuration/maltego/Maltego.v4.8.0.deb
 echo "Maltego installed"
 echo ""
 
+# Installing Maltego
+echo "====================="
+echo "Installing Maltego"
+echo "====================="
+echo "Installing BalenaEtcher"
+
+# Creating folder for installation of BalenaEtcher
+# echo "Creating folder for installation of BalenaEtcher"
+mkdir -p ~/configuration/balenaetcher
+
+# Downloading BalenaEtcher .deb package
+# echo "Downloading BalenaEtcher..."
+wget -P ~/configuration/balenaetcher/ https://github.com/balena-io/etcher/releases/download/v1.19.22/balena-etcher_1.19.22_amd64.deb
+
+# Installing BalenaEtcher
+# echo "Installing BalenaEtcher..."
+sudo dpkg -i ~/configuration/balenaetcher/balena-etcher_1.19.22_amd64.deb
+
+# Fixing missing dependencies, if any
+sudo apt-get install -f
+
+echo "BalenaEtcher installed"
+echo ""
+
 # Installing Obsidian
 echo "====================="
 echo "Installing Obsidian"
